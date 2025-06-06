7dd89bb1435f5ae7719a1828ff03c7be6475191d
SubratOjha35 <ojhasubrat35@gmail.com>
2025-06-06 12:34:32 +0530

    Enhance update support (#8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.4.0"
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
